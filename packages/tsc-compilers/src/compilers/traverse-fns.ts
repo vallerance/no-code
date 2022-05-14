00c3444b8652803f0b2330519b19329b4606a50b
Joshua Carter <carter.joshua.603@gmail.com>
2022-05-14 11:34:49 -0700

    Convert tsc-compilers package to typescript strict mode

--- packages/tsc-compilers/src/compilers/traverse-fns.ts
+++ packages/tsc-compilers/src/compilers/traverse-fns.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
 import * as ts from 'typescript';
 
 import { createProgram } from '../lib/bootstrap';
--- packages/tsc-compilers/src/main.ts
+++ packages/tsc-compilers/src/main.ts
@@ -3,11 +3,11 @@ import * as compilers from './compilers';
 
 console.log(process.argv);
 
-const compilerName = Case.camel(process.argv[2]);
+const compilerName = Case.camel(process.argv[2]) as keyof typeof compilers;
 
 compilers[compilerName](process.argv.slice(3));
 
-process.on('exit', (options, exitCode) => {
+process.on('exit', (options: unknown, exitCode: number) => {
     if (exitCode === 0) {
         console.log('exit');
     }
